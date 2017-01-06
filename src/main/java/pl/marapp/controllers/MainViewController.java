ef44a90e1656026fc0d74abc053b53ec680cf408
Sarseth <elune90@gmail.com>
2017-01-06 22:21:23 +0100

    Prepared controller, moved view to better resource dir

---
+++ src/main/java/pl/marapp/controllers/MainViewController.java
@@ -0,0 +1,4 @@
+package pl.marapp.controllers;
+
+public class MainViewController {
+}
--- src/main/java/pl/marapp/properties/WindowUrlProvider.java
+++ src/main/java/pl/marapp/properties/WindowUrlProvider.java
@@ -6,8 +6,10 @@ import java.net.URL;
 
 public class WindowUrlProvider {
 
+    private static final String VIEW_DICT = System.getProperty("user.dir") + "\\src\\main\\resources\\view\\";
+
 	public static URL getMainWindow() throws MalformedURLException {
-		return new File(System.getProperty("user.dir") + "\\src\\main\\resources\\MainView.fxml").toURL();
-	}
+        return new File(VIEW_DICT + "MainView.fxml").toURL();
+    }
 
 }
