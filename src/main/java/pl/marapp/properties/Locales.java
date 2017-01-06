fc57d51920b4e265a1e15a57949fc920562b73b6
Sarseth <elune90@gmail.com>
2017-01-06 22:04:05 +0100

    Working bundle i18n

---
+++ src/main/java/pl/marapp/properties/Locales.java
@@ -0,0 +1,11 @@
+package pl.marapp.properties;
+
+import java.util.Locale;
+
+public class Locales {
+
+    public static final Locale ENGLISH = Locale.ENGLISH;
+
+    public static final Locale POLISH = new Locale("Polish", "Poland");
+
+}
--- src/test/java/pl/marapp/I18nBundleTest.java
+++ src/test/java/pl/marapp/I18nBundleTest.java
@@ -3,9 +3,9 @@ package pl.marapp;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.Locale;
-
 import org.junit.Test;
+
+import pl.marapp.properties.Locales;
 
 public class I18nBundleTest {
 
@@ -13,13 +13,26 @@ public class I18nBundleTest {
     public void checkIfEnAppNameCorrect() {
         // GIVEN
         I18nBundle i18nBundle = I18nBundle.getInstance();
-        i18nBundle.changeLocale(Locale.ENGLISH);
+        i18nBundle.changeLocale(Locales.ENGLISH);
 
         // WHEN
         String appTitle = i18nBundle.getValue("appTitle");
 
         // THEN
         assertThat(appTitle, is("Client base"));
+    }
+
+    @Test
+    public void checkIfEnAppNameCorrectPolish() {
+        // GIVEN
+        I18nBundle i18nBundle = I18nBundle.getInstance();
+        i18nBundle.changeLocale(Locales.POLISH);
+
+        // WHEN
+        String appTitle = i18nBundle.getValue("appTitle");
+
+        // THEN
+        assertThat(appTitle, is("Baza klientów"));
     }
 
 }
