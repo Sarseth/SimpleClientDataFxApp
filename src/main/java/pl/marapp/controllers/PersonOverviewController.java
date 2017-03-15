8bcd90417655a8c52b9c40137fdb8991019a6773
Szymon <elune90@gmail.com>
2017-03-15 19:22:14 +0100

    Migration from base types to fx properties

--- src/main/java/pl/marapp/controllers/PersonOverviewController.java
+++ src/main/java/pl/marapp/controllers/PersonOverviewController.java
@@ -1,13 +1,25 @@
 package pl.marapp.controllers;
 
-import java.util.List;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import pl.marapp.Main;
 import pl.marapp.model.person.Person;
 
 public class PersonOverviewController {
 
-    private List<Person> personViewList;
+	private TableView<Person> personTableView;
 
+	private TableColumn<Person, String> firstNameColumn;
 
+	private TableColumn<Person, String> surenameColumn;
+
+	private Main mainApp;
+
+	@FXML
+	private void initialize() {
+		firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        surenameColumn.setCellValueFactory(cellData -> cellData.getValue().surnameProperty());
+	}
 
 }
--- src/main/java/pl/marapp/model/person/Person.java
+++ src/main/java/pl/marapp/model/person/Person.java
@@ -1,98 +1,134 @@
 package pl.marapp.model.person;
 
+import javafx.beans.property.*;
+
 import java.time.LocalDate;
 
 public class Person {
 
-    private long id;
+    private final LongProperty id = new SimpleLongProperty();
 
-    private String name;
+    private final StringProperty name = new SimpleStringProperty();
 
-    private String middleName;
+    private final StringProperty middleName = new SimpleStringProperty();
 
-    private String surname;
+    private final StringProperty surname = new SimpleStringProperty();
 
-    private Gender gender;
+    private final ObjectProperty<Gender> gender = new SimpleObjectProperty<>();
 
-    private LocalDate birthday;
+    private final ObjectProperty<LocalDate> birthday = new SimpleObjectProperty<>();
 
-    private LocalDate calculactedBirthday;
+    private final ObjectProperty<LocalDate> calculactedBirthday = new SimpleObjectProperty<>();
 
-    private int age;
+    private final IntegerProperty age = new SimpleIntegerProperty();
 
-    private Contact contact;
+    private final ObjectProperty<Contact> contact = new SimpleObjectProperty<>();
 
     public long getId() {
+        return id.get();
+    }
+
+    public LongProperty idProperty() {
         return id;
     }
 
     public void setId(long id) {
-        this.id = id;
+        this.id.set(id);
     }
 
     public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
 
     public String getMiddleName() {
+        return middleName.get();
+    }
+
+    public StringProperty middleNameProperty() {
         return middleName;
     }
 
     public void setMiddleName(String middleName) {
-        this.middleName = middleName;
+        this.middleName.set(middleName);
     }
 
     public String getSurname() {
+        return surname.get();
+    }
+
+    public StringProperty surnameProperty() {
         return surname;
     }
 
     public void setSurname(String surname) {
-        this.surname = surname;
+        this.surname.set(surname);
     }
 
     public Gender getGender() {
+        return gender.get();
+    }
+
+    public ObjectProperty<Gender> genderProperty() {
         return gender;
     }
 
     public void setGender(Gender gender) {
-        this.gender = gender;
+        this.gender.set(gender);
     }
 
     public LocalDate getBirthday() {
+        return birthday.get();
+    }
+
+    public ObjectProperty<LocalDate> birthdayProperty() {
         return birthday;
     }
 
     public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
+        this.birthday.set(birthday);
     }
 
     public LocalDate getCalculactedBirthday() {
+        return calculactedBirthday.get();
+    }
+
+    public ObjectProperty<LocalDate> calculactedBirthdayProperty() {
         return calculactedBirthday;
     }
 
     public void setCalculactedBirthday(LocalDate calculactedBirthday) {
-        this.calculactedBirthday = calculactedBirthday;
+        this.calculactedBirthday.set(calculactedBirthday);
     }
 
     public int getAge() {
+        return age.get();
+    }
+
+    public IntegerProperty ageProperty() {
         return age;
     }
 
     public void setAge(int age) {
-        this.age = age;
+        this.age.set(age);
     }
 
     public Contact getContact() {
+        return contact.get();
+    }
+
+    public ObjectProperty<Contact> contactProperty() {
         return contact;
     }
 
     public void setContact(Contact contact) {
-        this.contact = contact;
+        this.contact.set(contact);
     }
-
-
 }
